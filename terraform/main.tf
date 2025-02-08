5f5eee397c0a7ca659e39916ec14dadf4e2e31c9
divya21098 <kamathd37@gmail.com>
2025-02-08 11:50:43 +0530

    maintf

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,7 +7,42 @@ resource "aws_ecr_repository" "divya-reg" {
   }
 }
 
-data "aws_ecr_image" "service_image" {
-  repository_name = "my/service"
-  image_tag       = "latest"
+# data "aws_ecr_image" "service_image" {
+#   repository_name = "my/service"
+#   image_tag       = "latest"
+# }
+
+provider "aws" {
+  region = "us-west-1"
+}
+resource "aws_dynamodb_table" "state__lock_table" {
+  name         = "state-lock-bcvket"
+  hash_key     = "LockID"
+  billing_mode = "PAY_PER_REQUEST"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
+resource "aws_s3_bucket" "state_bucket" {
+  bucket = "divya-tf-state-bucket"
+  force_destroy = true
+
+}
+resource "aws_s3_bucket_versioning" "s3-version" {
+    bucket =aws_s3_bucket.state_bucket.id
+    versioning_configuration {
+      status =  "Enabled"
+    }
+  
+}
+resource "aws_s3_bucket_server_side_encryption_configuration" "example" {
+  bucket = aws_s3_bucket.state_bucket.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm     = "AES256"
+    }
+  }
 }

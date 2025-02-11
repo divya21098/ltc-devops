82c27673fb16a68d982961d8c4325b0032b5ac6f
divya21098 <kamathd37@gmail.com>
2025-02-11 14:24:55 +0530

    subnet cidr

--- terraform/k8.tf
+++ terraform/k8.tf
@@ -6,13 +6,13 @@ resource "aws_vpc" "main"{
 
 resource "aws_subnet" "subnet-1" {
   vpc_id = aws_vpc.main.id
-  cidr_block = "10.0.0.0/28"
+  cidr_block = "10.0.0.0/26"
   availability_zone = "us-west-1c"
   map_public_ip_on_launch = true
 }
 resource "aws_subnet" "subnet-2" {
   vpc_id = aws_vpc.main.id
-  cidr_block = "10.0.0.16/28"
+  cidr_block = "10.0.0.64/26"
   availability_zone = "us-west-1b"
   map_public_ip_on_launch = true
 }

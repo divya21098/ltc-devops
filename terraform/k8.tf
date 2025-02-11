185beb4d2d2b51acb90fa7bda656613f2786bcfa
divya21098 <kamathd37@gmail.com>
2025-02-11 17:30:20 +0530

    vpc create

--- terraform/k8.tf
+++ terraform/k8.tf
@@ -1,44 +1,44 @@
-# resource "aws_vpc" "main"{
-#     cidr_block = "10.0.0.0/16"
-#     enable_dns_hostnames = true
-# }
+resource "aws_vpc" "main"{
+    cidr_block = "10.0.0.0/16"
+    enable_dns_hostnames = true
+}
 
 
-# resource "aws_subnet" "subnet-1" {
-#   vpc_id = aws_vpc.main.id
-#   cidr_block = "10.0.0.0/20"
-#   availability_zone = "us-west-1c"
-#   map_public_ip_on_launch = true
-# }
-# resource "aws_subnet" "subnet-2" {
-#   vpc_id = aws_vpc.main.id
-#   cidr_block = "10.0.16.0/20"
-#   availability_zone = "us-west-1b"
-#   map_public_ip_on_launch = true
-# }
-# resource "aws_internet_gateway" "igw" {
-#     vpc_id = aws_vpc.main.id
+resource "aws_subnet" "subnet-1" {
+  vpc_id = aws_vpc.main.id
+  cidr_block = "10.0.0.0/20"
+  availability_zone = "us-west-1c"
+  map_public_ip_on_launch = true
+}
+resource "aws_subnet" "subnet-2" {
+  vpc_id = aws_vpc.main.id
+  cidr_block = "10.0.16.0/20"
+  availability_zone = "us-west-1b"
+  map_public_ip_on_launch = true
+}
+resource "aws_internet_gateway" "igw" {
+    vpc_id = aws_vpc.main.id
   
-# }
-# resource "aws_route_table" "rtab" {
-#   vpc_id = aws_vpc.main.id
-#   route {
-#     cidr_block = "0.0.0.0/0"
-#     gateway_id = aws_internet_gateway.igw.id
-#   }
-#   route {
-#     cidr_block = "10.0.0.0/24"
-#     gateway_id = "local"
-#   }
-# }
-# resource "aws_route_table_association" "subnet_1_association"{
-#     subnet_id = aws_subnet.subnet-1.id
-#     route_table_id = aws_route_table.rtab.id
-# }
-# resource "aws_route_table_association" "subnet_2_association"{
-#     subnet_id = aws_subnet.subnet-2.id
-#     route_table_id = aws_route_table.rtab.id
-# }
+}
+resource "aws_route_table" "rtab" {
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+  route {
+    cidr_block = "10.0.0.0/24"
+    gateway_id = "local"
+  }
+}
+resource "aws_route_table_association" "subnet_1_association"{
+    subnet_id = aws_subnet.subnet-1.id
+    route_table_id = aws_route_table.rtab.id
+}
+resource "aws_route_table_association" "subnet_2_association"{
+    subnet_id = aws_subnet.subnet-2.id
+    route_table_id = aws_route_table.rtab.id
+}
 # module "eks" {
 #   source  = "terraform-aws-modules/eks/aws"
 #   version = "~> 20.0"
--- terraform/provider.tf
+++ terraform/provider.tf
@@ -5,13 +5,13 @@ terraform {
         version = "~> 5.0"
     }
   }
-#    backend "s3"{
-#     bucket = "divya-tf-state-bucket"
-#     key = "tf-infra/terraform.tfstate"
-#     region = "us-west-1"
-#     dynamodb_table = "state-lock-bcvket"
-#     encrypt = true
-#   }
+   backend "s3"{
+    bucket = "divya-tf-state-bucket"
+    key = "tf-infra/terraform.tfstate"
+    region = "us-west-1"
+    dynamodb_table = "state-lock-bcvket"
+    encrypt = true
+   }
 }
 provider "aws" {
   region = "us-west-1"

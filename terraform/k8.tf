e7f622d1a940c0276bc1ece007f365918e3af6e7
divya21098 <kamathd37@gmail.com>
2025-02-11 19:08:00 +0530

    eks create

--- terraform/k8.tf
+++ terraform/k8.tf
@@ -43,7 +43,7 @@ module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "~> 20.0"
 
-  cluster_name    = "my-k8"
+  cluster_name    = "my-cluster"
   cluster_version = "1.31"
 
   bootstrap_self_managed_addons = false
